4db4d10aae2926227f61823d0c1d65586e6231d5
Juliuk <jxlxxs@gmail.com>
2026-06-03 20:47:11 +0200

    Joystick support

--- src/ariel-os-boards/src/stm32l496g-disco.rs
+++ src/ariel-os-boards/src/stm32l496g-disco.rs
@@ -3,7 +3,10 @@
 pub mod pins {
     use ariel_os_hal::hal::peripherals;
     ariel_os_hal::define_peripherals!(LedPeripherals { led0 : PB13, led1 : PA5, });
-    ariel_os_hal::define_peripherals!(ButtonPeripherals { button0 : PC13, });
+    ariel_os_hal::define_peripherals!(
+        ButtonPeripherals { button0 : PC13, button1 : PI8, button2 : PI10, button3 : PI9,
+        button4 : PF11, }
+    );
 }
 #[allow(unused_variables)]
 pub fn init(peripherals: &mut ariel_os_hal::hal::OptionalPeripherals) {}
